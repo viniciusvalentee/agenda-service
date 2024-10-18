3c1d433ce7968ca257d70c8d77cc4b4092f2517d
Vinicius Valente Ramirez <viniciusvramirez0108@gmail.com>
2024-10-18 13:04:24 -0300

    Fixed user admin creation

--- src/main/resources/db/migration/V4__insert_user_admin.sql
+++ src/main/resources/db/migration/V4__insert_user_admin.sql
@@ -1,2 +1,2 @@
 INSERT INTO usuario(id, nome, usuario, senha)
-VALUES(nextval('usuario_id_seq'), 'admin', 'admin', 'admin')
+VALUES(nextval('usuario_id_seq'), 'admin', 'admin', '$2a$10$spuYmIqTfhPQnKJFgxSP5eqqTA0qNzvNaDWOngiLRwqWYltgMVX6K');
